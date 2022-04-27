982f43dd186fcdc5f5ab909c6a6fe4773c0bf1ab
Marco Orlando <103572171+Marco-Orlando@users.noreply.github.com>
2022-04-27 12:01:50 +0200

    Update Main.java

--- src/it/meucci/Orlando/Main.java
+++ src/it/meucci/Orlando/Main.java
@@ -5,13 +5,13 @@ package it.meucci.Orlando;
  * Classe Main
  * 
  * @author Marco Orlando 4^CI
- * @version 2.0
+ * @version 2.0b online
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Prova 2");
+		System.out.println("Prova 2b online");
 	}
 
 }
