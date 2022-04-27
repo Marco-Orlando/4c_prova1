c79f908faa161d0e3c8b0c6ddca742a21fb3952e
Marco <marco.orlando04@gmail.com>
2022-04-27 11:45:51 +0200

    versione 1

---
+++ src/it/meucci/Orlando/Main.java
@@ -0,0 +1,17 @@
+package it.meucci.Orlando;
+
+/**
+ * 
+ * Classe Main
+ * 
+ * @author Marco Orlando 4^CI
+ * @version 1.0
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Prova 1");
+	}
+
+}
